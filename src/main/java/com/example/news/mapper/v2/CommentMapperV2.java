309adcf62c4ac963bc76242fd392619010837a15
ap.shimolin <ap.shimolin@gmail.com>
2024-07-04 11:46:01 +1000

    mapperV2 is modified

--- src/main/java/com/example/news/mapper/v2/CommentMapperV2.java
+++ src/main/java/com/example/news/mapper/v2/CommentMapperV2.java
@@ -4,10 +4,14 @@ import com.example.news.model.Comment;
 import com.example.news.web.model.CommentRequest;
 import com.example.news.web.model.CommentResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CommentMapperV2 {
+    @Mapping(source = "newsId", target = "news.id")
     Comment requestToComment(CommentRequest request);
-    CommentResponse commentToResponse(Comment comment);
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "news.id", target = "newsId")
+ CommentResponse commentToResponse(Comment comment);
 }
--- src/main/java/com/example/news/mapper/v2/NewsMapperV2.java
+++ src/main/java/com/example/news/mapper/v2/NewsMapperV2.java
@@ -4,10 +4,15 @@ import com.example.news.model.News;
 import com.example.news.web.model.NewsRequest;
 import com.example.news.web.model.NewsResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = {UserMapperV2.class})
 public interface NewsMapperV2 {
+
+    @Mapping(source = "categoryId", target = "category.id")
     News requestToNews(NewsRequest request);
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "category.id", target = "categoryId")
     NewsResponse newsToResponse(News news);
 }
